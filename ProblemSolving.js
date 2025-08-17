df48486305ea15e5180b2b5d7ff02600f9ab45b3
Debajit91 <debajitroy544@gmail.com>
2025-08-17 22:06:23 +0600

    Problem 2 Solved

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -1,10 +1,21 @@
 // Problem -1
 const str = "Hello World From JS";
 
-// const reverseString = (str) => [...str].reverse().join(""); (ideal for characters only)
+// const reverseString = (str) => [...str].reverse().join(""); (ideal for reversing characters only)
 const reverseString = (str) =>{
     const splitString = str.split(" ");
     const reverseStr = splitString.reverse().join(" ");
     return reverseStr;
 }
 // console.log(reverseString(str))
+
+
+// Problem -2
+const str2 = "Programming Hero";
+
+const countVowels = (str2)=>{
+    const vowel = 'aeiouAEIOU';
+
+    return [...str2].filter(char => vowel.includes(char)).length;
+}
+// console.log(countVowels(str2));
