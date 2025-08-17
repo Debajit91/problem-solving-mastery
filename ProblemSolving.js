0fb1a5534e79fea793fc1bcdfbcf235544583ea2
Debajit91 <debajitroy544@gmail.com>
2025-08-17 22:49:50 +0600

    problem 3 solved

--- ProblemSolving.js
+++ ProblemSolving.js
@@ -1,21 +1,30 @@
 // Problem -1
-const str = "Hello World From JS";
 
-// const reverseString = (str) => [...str].reverse().join(""); (ideal for reversing characters only)
 const reverseString = (str) =>{
     const splitString = str.split(" ");
     const reverseStr = splitString.reverse().join(" ");
     return reverseStr;
 }
-// console.log(reverseString(str))
+// console.log(reverseString("Hello World from JS"))
 
 
 // Problem -2
-const str2 = "Programming Hero";
 
-const countVowels = (str2)=>{
+const countVowels = (str)=>{
     const vowel = 'aeiouAEIOU';
 
-    return [...str2].filter(char => vowel.includes(char)).length;
+    return [...str].filter(char => vowel.includes(char)).length;
 }
-// console.log(countVowels(str2));
+// console.log(countVowels("Programming Hero"));
+
+
+// Problem -3
+
+const isPallindrome = (str) =>{
+    const cleaned = str.toLowerCase();
+    const reversedStr = cleaned.split("").reverse().join("");
+
+    return cleaned === reversedStr;
+}
+// console.log(isPallindrome('Madam'));
+// console.log(isPallindrome('hello'));
